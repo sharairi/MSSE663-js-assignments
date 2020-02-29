e2eb2dd9dbbac583e7ec6615808e1ed1130566b7
“sharairi” <smart_sharairi@hotmail.com>
2020-02-29 12:53:28 -0700

    week6

--- week6/src/higher-order-funcs.spec.ts
+++ week6/src/higher-order-funcs.spec.ts
@@ -1,8 +1,24 @@
 import { expect } from 'chai';
 import 'mocha';
+import { mappedThings, filteredThings } from './higher-order-funcs';
 
 describe('Higher Order Function tests', () => {
-  it("#1 should produce the expected result `[{ id: 1, name 'thing' }, { id: 2, name 'thing' }]` after hoisting", () => {});
+  it("#1 should produce the expected result `[{ id: 1, name 'thing' }, { id: 2, name 'thing' }]` after hoisting", () => {
 
-  it('#2 should return a single thing from array of things objects after hoisting and currying', () => {});
+    const expected = [
+      {
+        id: 1, name: 'thing1'
+      },
+      {
+        id: 2, name: 'thing2'
+
+      }
+    ]
+    expect(mappedThings).to.eql(expected);
+  });
+
+  it('#2 should return a single thing from array of things objects after hoisting and currying', () => {
+
+    expect(filteredThings('thing1')).to.eql([{ id: 1, name: 'thing1' }]);
+  });
 });
--- week6/src/higher-order-funcs.ts
+++ week6/src/higher-order-funcs.ts
@@ -12,8 +12,13 @@ const things = ['thing1', 'thing2'];
 
 // create callback here
 
+const callBack = (thing: string, i: number) => ({
+    id: i + 1,
+    name: thing
+}
+);
 // create map here
-
+export const mappedThings = things.map(callBack);
 /**
  * #2 Higher order then curry.
  *
@@ -21,5 +26,16 @@ const things = ['thing1', 'thing2'];
  * - Filter the results from #1 to return a single `thing` object.
  * - Hoist that filter callback and curry all functions.
  */
-
+interface Thing {
+    id: number,
+    name: string
+}
 // create function here
+const filter = <T>(comparitor: string) => (key: string) => (thing: T) =>
+    thing[key] === comparitor;
+
+export const filteredThings = <T>(comparitor: string) => (key: string) =>
+    mappedThings.filter(filter<Thing>(comparitor)(key));
+
+console.log(filteredThings('thing1')('name'));
+
